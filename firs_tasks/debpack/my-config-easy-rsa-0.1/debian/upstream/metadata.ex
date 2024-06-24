d8696883bfb494339776519f3e7ee742e4daddad
alex <kondratenkoa286@gmail.com>
2024-06-24 11:16:57 +0000

    my-first-project

---
+++ firs_tasks/debpack/my-config-easy-rsa-0.1/debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/my-config-easy-rsa/issues
+# Bug-Submit: https://github.com/<user>/my-config-easy-rsa/issues/new
+# Changelog: https://github.com/<user>/my-config-easy-rsa/blob/master/CHANGES
+# Documentation: https://github.com/<user>/my-config-easy-rsa/wiki
+# Repository-Browse: https://github.com/<user>/my-config-easy-rsa
+# Repository: https://github.com/<user>/my-config-easy-rsa.git
---
+++ firs_tasks/script_easy-rsa.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# Проверяем наличие Easy-RSA и, если не установлен, скачиваем его
+if [ ! -d "/usr/share/easy-rsa" ]; then
+    echo "Easy-RSA не установлен. Производится установка..."
+    sudo apt-get update
+    sudo apt-get install easy-rsa -y
+    if [ $? -ne 0 ]; then
+        echo "Ошибка: Не удалось установить Easy-RSA. Проверьте подключение к интернету или выполните установку вручную."
+        exit 1
+    fi
+    echo "Easy-RSA успешно установлен."
+fi
+
+# Создаем директорию для Easy-RSA
+mkdir ~/easy-rsa
+if [ $? -ne 0 ]; then
+    echo "Ошибка: Не удалось создать директорию ~/easy-rsa"
+    exit 1
+fi
+
+# Создаем символьную ссылку на установленные файлы Easy-RSA
+ln -s /usr/share/easy-rsa/* ~/easy-rsa/
+if [ $? -ne 0 ]; then
+    echo "Ошибка: Не удалось создать символьную ссылку на установленные файлы Easy-RSA"
+    exit 1
+fi
+
+# Устанавливаем права на директорию
+chmod 700 ~/easy-rsa/
+if [ $? -ne 0 ]; then
+    echo "Ошибка: Не удалось установить права на директорию ~/easy-rsa/"
+    exit 1
+fi
+
+# Переходим в директорию Easy-RSA
+cd ~/easy-rsa/
+if [ $? -ne 0 ]; then
+    echo "Ошибка: Не удалось перейти в директорию ~/easy-rsa/"
+    exit 1
+fi
+
+# Инициализируем инфраструктуру открытого ключа PKI
+./easyrsa init-pki
+if [ $? -ne 0 ]; then
+    echo "Ошибка: Не удалось инициализировать инфраструктуру PKI. Проверьте права доступа."
+    exit 1
+fi
+
+# Копируем файл vars.example в vars
+cp vars.example vars
+if [ $? -ne 0 ]; then
+    echo "Ошибка: Не удалось скопировать файл vars.example в vars"
+    exit 1
+fi
+
+# Создаем файл options с новыми параметрами
+cat << EOF > options
+set_var EASYRSA_REQ_COUNTRY    "RU"
+set_var EASYRSA_REQ_PROVINCE   "Moscow"
+set_var EASYRSA_REQ_CITY       "Moscow"
+set_var EASYRSA_REQ_ORG        "IT"
+set_var EASYRSA_REQ_EMAIL      "adminrsa@example.com"
+set_var EASYRSA_REQ_OU         "My_rsa_server"
+
+set_var EASYRSA_ALGO           ec
+set_var EASYRSA_DIGEST         "sha512"
+EOF
+
+# Добавляем новые параметры в файл vars
+cat options >> ~/easy-rsa/vars
+echo "Новые параметры EASYRSA_ALGO и EASYRSA_DIGEST успешно добавлены в файл vars"
+
+# Создаем центр сертификации
+./easyrsa build-ca
+if [ $? -ne 0 ]; then
+    echo "Ошибка: Не удалось создать центр сертификации"
+    exit 1
+fi
+
+echo "Скрипт выполнен успешно"
+

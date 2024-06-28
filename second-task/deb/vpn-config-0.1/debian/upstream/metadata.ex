85fe9409603faa7ef97009e4ab5315e8f883570c
alex <kondratenkoa286@gmail.com>
2024-06-28 09:28:42 +0000

    second-task-deb

---
+++ second-task/deb/vpn-config-0.1/debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/vpn-config/issues
+# Bug-Submit: https://github.com/<user>/vpn-config/issues/new
+# Changelog: https://github.com/<user>/vpn-config/blob/master/CHANGES
+# Documentation: https://github.com/<user>/vpn-config/wiki
+# Repository-Browse: https://github.com/<user>/vpn-config
+# Repository: https://github.com/<user>/vpn-config.git
